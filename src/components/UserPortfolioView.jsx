a69f93c204d09889a0f490b8afa2b91e91813a1d
Ellen Harris <ellenharris@Ellens-MacBook-Air.local>
2022-03-22 11:49:04 +0000

    debugging

--- src/components/UserPortfolioView.jsx
+++ src/components/UserPortfolioView.jsx
@@ -3,5 +3,5 @@ import { useState, useEffect } from "react";
 
 export default function UserPortfolioView(){
     const [companySelected, setCompanySelected]=useState();
-    return (<div><UserBanner /> portfolio view here <UserPortfolioCard/> </div>)
+    return (<div> portfolio view here <UserPortfolioCard/> </div>)
     }
