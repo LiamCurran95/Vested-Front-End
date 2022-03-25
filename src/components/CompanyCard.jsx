9708ef99dedcc04892c41222ced8e8784813a079
Ellen Harris <ellenharris@192.168.1.125>
2022-03-25 10:34:37 +0000

    trying to commit

--- src/components/CompanyCard.jsx
+++ src/components/CompanyCard.jsx
@@ -22,6 +22,7 @@ export default function CompanyCard() {
   if (isLoading) return <p>Loading...</p>;
   return (<main className="company-card">
     <section className="company-info">
+      <p>extra info (pls remove)</p>
     <img src={companyData.image.contentUrl}/>
     <section className="company-info-text">
       <h2>{company}</h2>
