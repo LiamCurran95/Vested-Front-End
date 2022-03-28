bfa665d8d96e4e232289a329dd50bd1b59f0bf6e
BillyZen <willjayandtay@hotmail.co.uk>
2022-03-28 10:25:13 +0100

    fixed bugs/set up portfolio function and form

--- src/util-functions.js
+++ src/util-functions.js
@@ -1,72 +1,72 @@
-export const generatePortfolio = async (env, soc, gov, user) => {
+// export const generatePortfolio = async (env, soc, gov, user) => {
 
-      const {username, formAnswers1, formAnswers2} = user
+//       const {username, formAnswers1, formAnswers2} = user
 
-      const esgData = await api.getEsgData()
+//       const esgData = await api.getEsgData()
 
-      const weightedEsg = esgData.map(company => {
-        const esgCompany = {}
-        esgCompany.ticker = company.ticker
-        esgCompany.company = company.name
-        esgCompany.score = ((company.socialScore*soc) + (company.governanceScore*gov) + (company.environmentScore*env))
+//       const weightedEsg = esgData.map(company => {
+//         const esgCompany = {}
+//         esgCompany.ticker = company.ticker
+//         esgCompany.company = company.name
+//         esgCompany.score = ((company.socialScore*soc) + (company.governanceScore*gov) + (company.environmentScore*env))
 
-        return esgCompany
-      })
+//         return esgCompany
+//       })
 
-      const polygonData = await api.getPolygonData()
-      const todaysStockData = await api.getTodaysStockData()
+//       const polygonData = await api.getPolygonData()
+//       const todaysStockData = await api.getTodaysStockData()
       
-      const marchPolygonData = polygonData.filter(stock => stock.date === "2020-03-01")
+//       const marchPolygonData = polygonData.filter(stock => stock.date === "2020-03-01")
 
-      const comparingCurrentToMarch = (today, march) => {
+//       const comparingCurrentToMarch = (today, march) => {
 
-        const scoredCompanies = today.map((company, index) => {
+//         const scoredCompanies = today.map((company, index) => {
           
-            if(company.averagePrice > march[index].averagePrice*1.5){
-              company.score = 2000
-            } else if (company.averagePrice > march[index].averagePrice*1.25) {
-              company.score = 1000
-            } else if (company.averagePrice < march[index].averagePrice*0.75) {
-              company.score = -1000
-            } else if (company.averagePrice < march[index].averagePrice*0.5) {
-              company.score = -2000
-            } else {
-              company.score = 0
-            }
+//             if(company.averagePrice > march[index].averagePrice*1.5){
+//               company.score = 2000
+//             } else if (company.averagePrice > march[index].averagePrice*1.25) {
+//               company.score = 1000
+//             } else if (company.averagePrice < march[index].averagePrice*0.75) {
+//               company.score = -1000
+//             } else if (company.averagePrice < march[index].averagePrice*0.5) {
+//               company.score = -2000
+//             } else {
+//               company.score = 0
+//             }
 
-          return company
-        })
-        return scoredCompanies
-      }
+//           return company
+//         })
+//         return scoredCompanies
+//       }
 
-      const scores = comparingCurrentToMarch(todaysStockData, marchPolygonData);
+//       const scores = comparingCurrentToMarch(todaysStockData, marchPolygonData);
 
-      const addingScoresToEsg = (scoreArr, esgArr) => {
-        const completedScores = esgArr.map((company, index) => {
-          company.score += scoreArr[index].score
-          return company
-        })
-        return completedScores
-      }
+//       const addingScoresToEsg = (scoreArr, esgArr) => {
+//         const completedScores = esgArr.map((company, index) => {
+//           company.score += scoreArr[index].score
+//           return company
+//         })
+//         return completedScores
+//       }
 
-      const completelyScoredData = addingScoresToEsg(scores, weightedEsg)
+//       const completelyScoredData = addingScoresToEsg(scores, weightedEsg)
 
-      const sortedCompletelyScoredData = completelyScoredData.sort((a, b) => (a.score > b.score) ? 1 : -1)
+//       const sortedCompletelyScoredData = completelyScoredData.sort((a, b) => (a.score > b.score) ? 1 : -1)
 
-      const portfolioCompanies = sortedCompletelyScoredData.slice(0, 5)
+//       const portfolioCompanies = sortedCompletelyScoredData.slice(0, 5)
 
-      const portfolioTickers = portfolioCompanies.map(entry => {
-        return entry.ticker
-      })
+//       const portfolioTickers = portfolioCompanies.map(entry => {
+//         return entry.ticker
+//       })
 
-      const portfolioNames = portfolioCompanies.map(entry => {
-        return entry.company
-      })
+//       const portfolioNames = portfolioCompanies.map(entry => {
+//         return entry.company
+//       })
 
-      const answers = formAnswers1.length === 0 ? "formAnswers1" : formAnswers2.length === 0 ? "formAnswers2" : "formAnswers3"
+//       const answers = formAnswers1.length === 0 ? "formAnswers1" : formAnswers2.length === 0 ? "formAnswers2" : "formAnswers3"
 
-      await api.updateUserFormAnswers(username, answers, env, soc, gov)
-      await api.updatePortfolioOfUser(portfolioTickers, username)
+//       await api.updateUserFormAnswers(username, answers, env, soc, gov)
+//       await api.updatePortfolioOfUser(portfolioTickers, username)
 
-      return portfolioNames
-    }
+//       return portfolioNames
+//     }
