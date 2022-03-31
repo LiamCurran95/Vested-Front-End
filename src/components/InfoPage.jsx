58e62fbaa47c3dfadb3a74942c80dfcab851c5b5
LiamCurran <LiamCurran95>
2022-03-31 14:25:32 +0100

    changing chart and desktop info page

--- src/components/InfoPage.jsx
+++ src/components/InfoPage.jsx
@@ -1,60 +1,169 @@
-export default function InfoPage(){
-    return (
-    <main className="info-page">
+export default function InfoPage() {
+	return (
+		<main className="info-page">
+			<h1>What is ESG?</h1>
+			<h2>Environmental, Social, and Governance Criteria</h2>
 
-        <h1>What is ESG?</h1>
-        <h2>Environmental, Social, and Governance Criteria</h2>
+			<img
+				className="info-illustration"
+				src="https://wealthofgeeks.com/wp-content/uploads/2020/09/ESGInvesting.png"
+				alt="illustration of woman sitting on stack of coins looking at her computer; ESG superimposed over upwards trending graph and clouds"
+			/>
+			<br></br>
+			<cite>
+				<a
+					className="cite"
+					href="https://wealthofgeeks.com/wp-content/uploads/2020/09/ESGInvesting.png"
+				>
+					Source
+				</a>
+			</cite>
 
-        <img className="info-illustration" src="https://wealthofgeeks.com/wp-content/uploads/2020/09/ESGInvesting.png" alt="illustration of woman sitting on stack of coins looking at her computer; ESG superimposed over upwards trending graph and clouds" />
-        <br></br>
-        <cite><a className="cite" href="https://wealthofgeeks.com/wp-content/uploads/2020/09/ESGInvesting.png">Source</a></cite>
-        
-        <ul className="info-esg-list">
-    
-            <section>
-            <h1>E</h1>
-            <dl>E, environmental criteria, includes the energy your company takes in and the waste it discharges, the resources it needs, and the consequences for living beings as a result. Not least, E encompasses carbon emissions and climate change. Every company uses energy and resources; every company affects, and is affected by, the environment.</dl>
-            </section>
-            
-            <section>
-            <h1>S</h1>
-            <dl>S, social criteria, addresses the relationships your company has and the reputation it fosters with people and institutions in the communities where you do business. S includes labor relations and diversity and inclusion. Every company operates within a broader, diverse society.</dl>
-            </section>
-
-            <section>
-            <h1>G</h1>
-            <dl>G, governance, is the internal system of practices, controls, and procedures your company adopts in order to govern itself, make effective decisions, comply with the law, and meet the needs of external stakeholders. Every company, which is itself a legal creation, requires governance.</dl>
-            </section>
-            
-            <cite> <a className="cite" href="https://www.mckinsey.com/business-functions/strategy-and-corporate-finance/our-insights/five-ways-that-esg-creates-value">[McKinsey & Company]</a></cite>
-        
-            <dl>
-                Vested uses ESG scores as a criteria to evaluate the relative compatibility of a company with the values that each user selects as important for them when generating a portfolio. We sort through publicly listed companies to suggest a list of potential stocks that have ESG scores that are best matched to the users selections, and allow the user to 'monitor' the performance of those stocks over time in a simulated model 
-            </dl>
-
-            <dl>
-                Environmental, social, and governance (ESG) criteria are an increasingly popular way for investors to evaluate companies in which they might want to invest. Many mutual funds, brokerage firms, and robo-advisors now offer products that employ ESG criteria.ESG criteria can also help investors avoid companies that might pose a greater financial risk due to their environmental or other practices. 
-            </dl>
-            <cite > <a className="cite" href="https://www.investopedia.com/terms/e/environmental-social-and-governance-esg-criteria.asp"> [Investopedia]</a></cite>
-
-
-            <dl>
-                There is not a standardised approach to the calculation or presentation of different ESG metrics. Investors can employ a variety of analytical approaches and data sources to address ESG considerations, including weighting to client interest and potential value. Understanding the relative merits and limitations of different metrics can help to form a more complete picture of ESG risks and opportunities. 
-            </dl>
-            <cite > <a className="cite" href="https://www.cfainstitute.org/en/research/esg-investing"> [CFA Institute] </a></cite>
-        
-       
-            Here are some resources to learn more about ESG criteria and their uses in corporate governance:
-            <li > <a className="cite" href="https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/835214/Sep_2019_bulletin.pdf">GOV.uk</a></li>
-            <li> <a className="cite" href="https://www.mckinsey.com/business-functions/strategy-and-corporate-finance/our-insights/five-ways-that-esg-creates-value">McKinsey & Company</a></li>
-            <li> <a className="cite" href="https://www.oecd.org/finance/esg-investing.htm">OECD</a></li>
-            <li> <a className="cite" href=" https://normative.io/insight/esg-investing-is-gold/">Normative</a></li>
-            <li> <a className="cite"href="https://www.forbes.com/advisor/investing/esg-investing/">Forbes</a></li>
-            <li> <a className="cite"href="https://www.investopedia.com/terms/e/environmental-social-and-governance-esg-criteria.asp">Investopedia</a></li>
-        </ul>
-
-
-
-
-    </main>)
-    }
+			<ul className="info-esg-list">
+				<section>
+					<h1>E</h1>
+					<dl>
+						Environmental governance is a concept in political ecology,
+						environmental policy and human geography that advocates sustainable
+						development as the supreme consideration for managing all human
+						activities — political, social and economic. <br />
+						<br />
+						Every company uses energy and resources; every company affects, and
+						is affected by, the environment.
+					</dl>
+				</section>
+				<section>
+					<h1>S</h1>
+					<dl>
+						Social governance addresses the relationships your company has and
+						the reputation it fosters with people and institutions in the
+						communities where you do business. Namely, labor relations and
+						diversity and inclusion. Every company operates within a broader,
+						diverse society.
+					</dl>
+				</section>
+				<section>
+					<h1>G</h1>
+					<dl>
+						Governance refers to the internal system of practices, controls, and
+						procedures your company adopts in order to govern itself, make
+						effective decisions, comply with the law, and meet the needs of
+						external stakeholders. Every company, which is itself a legal
+						creation, requires governance.
+					</dl>
+				</section>
+				<cite>
+					{" "}
+					<a
+						className="cite"
+						href="https://www.mckinsey.com/business-functions/strategy-and-corporate-finance/our-insights/five-ways-that-esg-creates-value"
+					>
+						[McKinsey & Company]
+					</a>
+				</cite>
+				<dl>
+					Vested uses ESG scores as a criteria to evaluate the relative
+					compatibility of a company with the values that each user selects as
+					important for them when generating a portfolio. We sort through
+					publicly listed companies to suggest a list of potential stocks that
+					have ESG scores that are best matched to the users selections, and
+					allow the user to 'monitor' the performance of those stocks over time
+					in a simulated model
+				</dl>
+				<dl>
+					Environmental, social, and governance (ESG) criteria are an
+					increasingly popular way for investors to evaluate companies in which
+					they might want to invest. Many mutual funds, brokerage firms, and
+					robo-advisors now offer products that employ ESG criteria.ESG criteria
+					can also help investors avoid companies that might pose a greater
+					financial risk due to their environmental or other practices.
+				</dl>
+				<cite>
+					{" "}
+					<a
+						className="cite"
+						href="https://www.investopedia.com/terms/e/environmental-social-and-governance-esg-criteria.asp"
+					>
+						{" "}
+						[Investopedia]
+					</a>
+				</cite>
+				<dl>
+					There is not a standardised approach to the calculation or
+					presentation of different ESG metrics. Investors can employ a variety
+					of analytical approaches and data sources to address ESG
+					considerations, including weighting to client interest and potential
+					value. Understanding the relative merits and limitations of different
+					metrics can help to form a more complete picture of ESG risks and
+					opportunities.
+				</dl>
+				<cite>
+					{" "}
+					<a
+						className="cite"
+						href="https://www.cfainstitute.org/en/research/esg-investing"
+					>
+						{" "}
+						[CFA Institute]{" "}
+					</a>
+				</cite>
+				Here are some resources to learn more about ESG criteria and their uses
+				in corporate governance:
+				<li>
+					{" "}
+					<a
+						className="cite"
+						href="https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/835214/Sep_2019_bulletin.pdf"
+					>
+						GOV.uk
+					</a>
+				</li>
+				<li>
+					{" "}
+					<a
+						className="cite"
+						href="https://www.mckinsey.com/business-functions/strategy-and-corporate-finance/our-insights/five-ways-that-esg-creates-value"
+					>
+						McKinsey & Company
+					</a>
+				</li>
+				<li>
+					{" "}
+					<a
+						className="cite"
+						href="https://www.oecd.org/finance/esg-investing.htm"
+					>
+						OECD
+					</a>
+				</li>
+				<li>
+					{" "}
+					<a
+						className="cite"
+						href=" https://normative.io/insight/esg-investing-is-gold/"
+					>
+						Normative
+					</a>
+				</li>
+				<li>
+					{" "}
+					<a
+						className="cite"
+						href="https://www.forbes.com/advisor/investing/esg-investing/"
+					>
+						Forbes
+					</a>
+				</li>
+				<li>
+					{" "}
+					<a
+						className="cite"
+						href="https://www.investopedia.com/terms/e/environmental-social-and-governance-esg-criteria.asp"
+					>
+						Investopedia
+					</a>
+				</li>
+			</ul>
+		</main>
+	);
+}
